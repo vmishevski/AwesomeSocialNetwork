87fc978fd5f152e42c250956f222a25e53445866
Voislav Mishevski <voislav.mishevski@gmail.com>
2015-10-24 03:41:55 +0200

    Livereload. FINALLY.

--- AwesomeSocialNetwork/Gulpfile.js
+++ AwesomeSocialNetwork/Gulpfile.js
@@ -1,4 +1,23 @@
 ﻿var gulp = require('gulp');
 var livereload = require('gulp-livereload');
-var nodemon = require('nodemon');
+var nodemon = require('gulp-nodemon');
 
+var watchLivereload = function () {
+    gulp.watch(['public/**/*.js'], function (event) {
+        gulp.src(event.path, {read: false})
+            .pipe(livereload());
+    });
+};
+
+gulp.task('serve', function () {
+    nodemon({
+        script: 'bin/www',
+        ignore: ['public/**', 'spec/**']
+    }).on('start', function () {
+        livereload.reload();
+        livereload.listen();
+        watchLivereload();
+    });
+});
+
+gulp.task('default', ['serve']);
