abb32f108ff834ba156e6d25b0441827a3dcf153
Voislav Mishevski <voislav.mishevski@gmail.com>
2015-10-28 21:46:31 +0100

    mocha trial

---
+++ test/remove-me.js
@@ -0,0 +1,9 @@
+require('../server-inst/model/index');
+
+describe('mocha', function () {
+    it('should pass', function () {
+        var expect = require('chai').expect;
+
+        expect(1).to.equal(1);
+    })
+});
