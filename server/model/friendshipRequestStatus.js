2df124c8117ba5a9b3bcd0d302c4771e64fe3608
Voislav Mishevski <voislav.mishevski@gmail.com>
2015-11-03 23:50:34 +0100

    Timeline schema. Define friendship request and friend list.

---
+++ server/model/friendshipRequestStatus.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Voislav on 11/3/2015.
+ */
+
+module.exports = {
+    pending: 1,
+    accepted: 2,
+    rejected: 3
+};
---
+++ server/model/timelineModel.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Voislav on 11/3/2015.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var TimelineSchema = new Schema({
+    userId: {type: Schema.Types.ObjectId, required: true},
+    friendshipRequests: [
+        {
+            userId: {type: Schema.Types.ObjectId, required: true},
+            dateRequested: {type: Date, default: Date.now},
+            status: {type: Number, min: 1, max: 3, default: 1}
+        }
+    ],
+    friends: {
+        userId: {type: Schema.Types.ObjectId, required: true},
+        dateBecomeFriends: {type: Date, default: Date.now}
+    }
+});
+
+mongoose.model('Timeline', TimelineSchema);
